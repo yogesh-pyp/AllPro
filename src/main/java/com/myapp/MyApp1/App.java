b1411d4ed2ae71131f655d8bf138fae449f4bf42
admin <admin@DESKTOP-BA2C3R8>
2018-07-15 00:24:39 +0530

    comment_17(Note:local branch created from remote origin/master having commit message comment_15 which inconsitent or old to local master)

--- src/main/java/com/myapp/MyApp1/App.java
+++ src/main/java/com/myapp/MyApp1/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	
-        System.out.println( "Hello India!" ); //comment_15
+        System.out.println( "Hello India!" ); //comment_17
 
        
         System.out.println( "Welcome to java!" );
