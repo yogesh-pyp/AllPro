dabb6a09af86e73d5d3813c92e7fab8bff71a2cf
admin <admin@DESKTOP-BA2C3R8>
2018-07-14 19:03:05 +0530

    2nd comment added

--- src/main/java/com/myapp/MyApp1/App.java
+++ src/main/java/com/myapp/MyApp1/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello India!" );
+        System.out.println( "Hello India!" );//comment two
         System.out.println( "Welcome to java!" );
     }
 }
